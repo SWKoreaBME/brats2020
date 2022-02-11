5316f5ef71973be94d802ca56ee8c2570cab41c7
Sangwook Kim <sangwook.kim@mail.utoronto.ca>
2022-02-10 20:50:03 -0500

    new feature: model parameter counter

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,4 +1,5 @@
 import torch
+import pandas as pd
 
 
 def concat_bg(tensor):
@@ -14,6 +15,24 @@ def concat_bg(tensor):
     bg_tensor[torch.where(tensor.sum(1) == 0)] = 1
     out_tensor = torch.cat([bg_tensor.unsqueeze(1), tensor], 1).to(tensor.device)
     return out_tensor
+
+
+def count_parameters(model):
+    """Calculate the number of paramters of a given model
+    Code from viswanatha_reddy
+    https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/24
+    """
+    table = list()
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        param = parameter.numel()
+        table.append([name, param])
+        total_params+=param
+    table_df = pd.DataFrame(data=table, columns=["Modules", "Parameters"])
+    print(f"Total Trainable Params: {total_params}")
+    return table_df, total_params
 
 
 if __name__== "__main__":
