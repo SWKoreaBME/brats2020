c365c39c4f03c008e9e117379e67115fc616aae8
Sangwook Kim <sangwook.kim@mail.utoronto.ca>
2022-02-15 12:14:53 -0500

    update: Removed input, Metric class updated

---
+++ remove_input.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1, 2, 3"
+
+from time import time
+from copy import copy
+from utils.Visualize import plot_with_hist, plot_whole_imgs
+from utils.Utils import concat_bg
+from utils.Data import convert_label_to_brats
+
+
+def remove_input(tensor, axis=0):
+    """Remove a single input modality
+        
+        input tensor: B x C x H x W
+        ouptut tensor: B x C x H x W
+    """
+    output_tensor = torch.zeros_like(tensor)
+    single_size = (tensor.size(0), *tensor.size()[2:])
+    for i in range(output_tensor.size(1)):
+        if i == axis:
+            output_tensor[:, i] = torch.zeros(single_size)
+        else:
+            output_tensor[:, i] = tensor[:, i]
+    return output_tensor
+
+
+if __name__ == "__main__":
+    
+    from utils.Device import gpu_setting, model_dataparallel
+    from utils.Model import load_model_weights
+    from utils.Data import load_dataloader
+    from utils.Uncertainty import get_dropout_uncertainty
+    from monai.networks.nets import UNETR, UNet
+    
+    import monai.transforms as tf
+    
+    sigmoid = nn.Sigmoid()
+    
+    # Model setting
+    amp = True
+    device, multi_gpu = gpu_setting()
+    # model = UNETR(in_channels=4,
+    #               out_channels=4,
+    #               img_size=240,
+    #               feature_size=8,
+    #               dropout_rate=0.3,
+    #               hidden_size=64,
+    #               num_heads=4,
+    #               num_layers=4,
+    #               mlp_dim=128,
+    #               norm_name='instance',
+    #               spatial_dims=2).to(device)
+    model = UNet(
+        spatial_dims=2,
+        in_channels=4,
+        out_channels=3+1,
+        channels=(8, 16, 32, 64),
+        strides=(2, 2, 2, 2),
+        act="RELU",
+        norm='instance',
+        dropout=0.3,
+        bias=True
+    ).to(device)
+    ckpt_dir = "./result/exps/unet-merge-4layer-noaug"
+    model_weights = os.path.join(ckpt_dir, "best.pth")
+    model = load_model_weights(model, model_weights, dp=False)
+    model = model_dataparallel(model, multi_gpu)
+    
+    test_loader_params = dict(
+        batch_size=56,
+        shuffle=True
+    )
+    
+    # validation and test transforms
+    val_transforms = tf.Compose([
+        tf.LoadImaged(reader="NibabelReader", keys=['image', 'label']),
+        # tf.AsDiscreted(keys=['label'], threshold_values=True),
+        tf.EnsureTyped(keys=["image", "label"]),
+        tf.ToTensord(keys=['image', 'label']),
+        tf.ToDeviced(keys=["image", "label"], device=device)
+    ])
+    
+    # img_save_dir = "./result/exps/unet-noaug/uncertainty/dropout"
+    img_save_dir = os.path.join(ckpt_dir, "input_removed")
+    root_dir = "/cluster/projects/mcintoshgroup/BraTs2020/data_monai/"
+    os.makedirs(img_save_dir, exist_ok=True)
+
+    test_dataloader = load_dataloader(root_dir, "test", val_transforms, test_loader_params)
+    
+    
+    # Image seqs
+    images_seqs = ["T1", "T1Gd", "T2", "FLAIR"]
+    tumors_names = ["TC", "WT", "ET"]
+    
+    
+    # Iterate through mini-batches
+    for batch_idx, batch in enumerate(test_dataloader):
+        inputs, labels = batch["image"], convert_label_to_brats(concat_bg(batch["label"]))  # use all four mods
+        labels_np = labels.detach().cpu().numpy()
+        inputs = inputs.to(device)
+        
+        # remove
+        with torch.cuda.amp.autocast():
+            outputs = torch.where(sigmoid(model(inputs)) > 0.5, 1, 0)
+            
+            plot_whole_imgs(inputs[:, 0].detach().cpu().numpy(), os.path.join(img_save_dir, f"inputs.jpg"))
+            plot_whole_imgs(labels[:, 1:].sum(1).detach().cpu().numpy(), os.path.join(img_save_dir, f"labels.jpg"))
+            plot_whole_imgs(outputs[:, 1:].sum(1).detach().cpu().numpy(), os.path.join(img_save_dir, f"outputs.jpg"))
+            
+            for i in range(4):  # Iterate over image sequences
+                removed_input = remove_input(inputs, i)
+                removed_outputs = torch.where(sigmoid(model(removed_input)) > 0.5, 1, 0)
+                removed_mean, removed_std = get_dropout_uncertainty(model, removed_input, labels, num_iters=10, vis=False)
+                removed_uncertainty = removed_mean + removed_std
+                
+                plot_whole_imgs(removed_outputs[:, 1:].sum(1).detach().cpu().numpy(), os.path.join(img_save_dir, f"{images_seqs[i]}_removed.jpg"))
+                for k in range(labels.size(1)):
+                    if k == 0:
+                        continue
+                    plot_whole_imgs(removed_uncertainty[:, k],
+                                    os.path.join(img_save_dir, f"{images_seqs[i]}_removed_uncertainty_{tumors_names[k-1]}.jpg"))
+        break
--- utils/Metric.py
+++ utils/Metric.py
@@ -1,6 +1,11 @@
+from xml.etree.ElementInclude import include
 import torch
+import pandas as pd
 import torch.nn as nn
+import os
+
 from monai.networks import one_hot
+from monai.metrics import get_confusion_matrix
 
 
 def get_dice_score(predict: torch.Tensor, target: torch.Tensor, smooth=1e-4, p=2):
@@ -39,12 +44,13 @@ def compute_meandice(seg_out, seg_label, include_background=False):
     return seg_batch_score
 
 
-def compute_meandice_multilabel(seg_out, seg_label, include_background=False):
+def compute_meandice_multilabel(seg_out, seg_label, include_background=False, sigmoid=True):
     """ Segmentation score for non-softmax output with multi-labels
     """
     num_labels = seg_out.size(1)
     sigmoid = nn.Sigmoid()
-    seg_out = torch.where(sigmoid(seg_out) > 0.5, 1, 0)
+    if sigmoid:
+        seg_out = torch.where(sigmoid(seg_out) > 0.5, 1, 0)
     
     seg_batch_score = 0
     for label_idx in range(num_labels):
@@ -60,6 +66,70 @@ def compute_meandice_multilabel(seg_out, seg_label, include_background=False):
     else:
         seg_batch_score /= (num_labels - 1)
     return seg_batch_score
+
+
+class Score:
+    def __init__(self, include_background=False, sigmoid=False, softmax=False):
+        self.total_dice = 0
+        self.total_conf_matrix = None
+        self.sigmoid_fn = nn.Sigmoid()
+        self.sigmoid = sigmoid
+        self.include_background = include_background
+        self.total_num_imgs = 0
+        self.total_data = list()
+        self.tumor_names = ["Tumor Core (TC)", "Whole Tumor (WT)", "Enhancing Tumor (ET)"]
+    
+    @staticmethod
+    def get_sen_spe(conf_matrix):
+        tp, fp, tn, fn = conf_matrix
+        sensitivity = tp / (tp + fn)
+        specificity = tn / (tn + fp)
+        return sensitivity, specificity
+        
+    def save(self, ckpt_save_dir):
+        dice_score = self.total_dice / self.total_num_imgs
+        conf_matrix_sum = self.total_conf_matrix.sum(0)
+        
+        mean_sen, mean_spe = 0, 0
+        for label_idx in range(conf_matrix_sum.size(0)):
+            sen, spe = self.get_sen_spe(conf_matrix_sum[label_idx])
+            mean_sen += sen
+            mean_spe += spe
+            
+        mean_sen = mean_sen / len(self.tumor_names)
+        mean_spe = mean_spe / len(self.tumor_names)
+        
+        print(f"Dice: {dice_score:.4f}")
+        print(f"Sensitivity: {mean_sen:.4f}")
+        print(f"Specificity: {mean_spe:.4f}")
+        
+        self.total_data.append([float(dice_score), float(mean_sen), float(mean_spe)])
+        pd.DataFrame(self.total_data, 
+                     columns=["Dice", "Sensitivity", "Specificity"]).to_csv(os.path.join(ckpt_save_dir, "test.csv"))
+    
+    def evaluate(self, y_pred, y_true):
+        if self.sigmoid:
+            y_pred = torch.where(self.sigmoid_fn(y_pred) > 0.5, 1, 0)
+        
+        y_pred = y_pred.detach().cpu()
+        y_true = y_true.detach().cpu()
+        dice_score = compute_meandice_multilabel(y_pred,
+                                                 y_true, 
+                                                 include_background=self.include_background, 
+                                                 sigmoid=False) * y_pred.size(0)
+        
+        conf_matrix = get_confusion_matrix(y_pred,
+                                           y_true,
+                                           include_background=self.include_background).detach().cpu()
+        
+        if self.total_conf_matrix is None:
+            self.total_conf_matrix = conf_matrix
+        else:
+            self.total_conf_matrix = torch.cat((conf_matrix, self.total_conf_matrix), 0)
+        
+        self.total_dice += dice_score
+        self.total_num_imgs += y_pred.size(0)
+    
 
 if __name__ == "__main__":
     pass
